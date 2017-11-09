cb2477e4afe79f3e1ae2c7cbec4804590da08ea6
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-09 17:24:07 +0100

    finished with no layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
-  resources:cocktails, only:[:index, :show, :new ,:create] do
-    resources:doses, only:[:destroy, :new, :create]
+  resources:cocktails, only:[:index, :show, :new, :create] do
+    resources:doses, only:[:new, :create]
   end
+  delete '/doses/:id', to: 'doses#destroy'
 end
