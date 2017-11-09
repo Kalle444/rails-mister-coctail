a564c17e252fca69c3d3a45da139768e376c2031
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-09 14:31:33 +0100

    cotrollers added

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
+gem 'pry-byebug'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.6'
 # Use postgresql as the database for Active Record
@@ -47,6 +47,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem "better_errors"
+  gem "binding_of_caller"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "start seed"
+
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+puts "db's dropped"
+
+
+# first dose
+first = Dose.new(description: "6cl")
+first.cocktail = Cocktail.create(name: "Mojito")
+first.ingredient = Ingredient.create(name: "mint leaves")
+first.save
+
+# second dose
+second = Dose.new(description: "4cl")
+second.cocktail = Cocktail.create(name: "GT")
+second.ingredient = Ingredient.create(name: "ice")
+second.save
+
+# third dose
+third = Dose.new(description: "8cl")
+third.cocktail = Cocktail.create(name: "Screwdriver")
+third.ingredient = Ingredient.create(name: "lemon")
+third.save
+
+puts "seeded complete"
